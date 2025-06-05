635d1ebb37e0f0e45c6997f6f561b996f3e3c14b
Naitik Pal <naitikpal513@gmail.com>
2025-06-05 23:24:41 +0530

    Clean Up of INdex.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,82 +1,14 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv"
-
-dotenv.config({
-  path:"./env"
-})
-
-const app = express();
-
-app.use(
-    cors({
-      credentials: true,
-      origin: "*",
-    })
-  );
-app.use(cookieParser());
-
-app.use(express.json());
-
-app.use(express.urlencoded({extended:true}));
-
-const users = [];
-
-app.post("/signup", (req, res) => {
-    const { first_name, last_name, Email, e_pw, c_pw } = req.body;
-    console.log(req.body);
-
-  if (!first_name || !last_name || !Email || !e_pw || !c_pw) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  if (!Email.endsWith("@gmail.com")) {
-    return res.status(400).json({ messgae: "Invalid Email" });
-  }
-  if (e_pw.length < 8) {
-    return res.status(400)({
-      messgae: "Password must be at least 8 character long",
-    });
-  }
-  const userExists = users.find((user) => user.Email === Email);
-
-  if (userExists) {
-    return res.status(400).json({ messgae: "Email already registered" });
-  }
-  const newUser = { first_name, last_name, Email, e_pw, c_pw };
-  users.push(newUser);
-
-  res.status(201).json({ message: "Signup Successful" });  //, user: newUser
-});
-
-app.post("/login", (req, res) => {
-  const { Email, e_pw } = req.body;
-
-  const existingUser = users.find(
-    (user) => user.Email === email && user.password === password
-  );
-
-  if (!existingUser) {
-    return res.status(401).json({ messgae: "Invalid email or password" });
-  }
-  res.status(200).json({ message: "Login Successful", user: existingUser });
-});
 import dotenv from "dotenv";
 import { app } from "./app.js";
+
 dotenv.config({
   path: "./env",
 });
-
 
 app.listen(process.env.PORTPORT || 8000, () => {
   console.log(`Server is running on local host ${process.env.PORT||8000}`);
 });
 
 
-
-
-app.listen(process.env.PORTPORT || 8000, () => {
-  console.log(`Server is running on local host ${process.env.PORT || 8000}`);
-});
 
 
