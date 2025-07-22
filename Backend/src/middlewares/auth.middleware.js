ccbaa7c69cbde4ca8492f3d4811695be4d28fc51
Naitik Pal <naitikpal513@gmail.com>
2025-07-22 16:41:59 +0530

    Add Auth Middlewares

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {User} from '../models/user.model.js';
 import { ApiError } from '../utils/ApiError.js';
-import { jwt } from "jsonwebtoken";
+import jwt  from  "jsonwebtoken";
 
 
 export const authMiddleware = asyncHandler( async (req, _, next ) => {
