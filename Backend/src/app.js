b677a4b07188cb2f89a6b9ecef290ebe46151837
Naitik Pal <naitikpal513@gmail.com>
2025-07-22 08:23:12 +0530

    Add Api Routes for User Upload subjects and Dashboard

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
+import uploadRouter from "./routes/upload.route.js" 
+import dashboardRouter from "./routes/dashboard.route.js"
 // import scheduleRouter from "./routes/schedule.routes.js"
 
 const app = express()
@@ -17,9 +19,11 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/users",userRouter)
+app.use("/api/v1/users",userRouter)
 
-// app.use("/schedule",scheduleRouter)
+app.use("/api/v1/upload", uploadRouter) 
+
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 
